98761a3f12c5c6a8a657e755f84e5045a9d4265b
YeHtutAung <yehaung.nov@gmail.com>
2022-12-27 15:01:51 +0100

    JS of QuizApp running

--- script.js
+++ script.js
@@ -23,6 +23,62 @@ let questions = [
         "answer_4": "Anguilla",
         "right_answer": 1
     },
+    {
+        "question": "Was ist das größte Tier der Welt?",
+        "answer_1": "Elefant",
+        "answer_2": "Blauwal",
+        "answer_3": "Braunbär",
+        "answer_4": "Giraffe",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie viele Stacheln hat ein Igel ungefähr?",
+        "answer_1": "50",
+        "answer_2": "500",
+        "answer_3": "1000",
+        "answer_4": "5000",
+        "right_answer": 4
+    },
+    {
+        "question": "Welches Land hat die höchste Migrationsrate?",
+        "answer_1": "Spanien",
+        "answer_2": "Italien",
+        "answer_3": "Niederlande",
+        "answer_4": "Japan",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche Stadt is die Hauptstadt Spaniens?",
+        "answer_1": "Barcelona",
+        "answer_2": "Valencia",
+        "answer_3": "Andorra la Vella",
+        "answer_4": "Madrid",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie heißt der höchste Berg der Welt?",
+        "answer_1": "Alpen",
+        "answer_2": "Zugspitze",
+        "answer_3": "Mount Everest",
+        "answer_4": "Kilimandscharo",
+        "right_answer": 3
+    },
+    {
+        "question": "Was Essen Bären sehr gerne?",
+        "answer_1": "Honig",
+        "answer_2": "Marmelade",
+        "answer_3": "Nussnougatcreme",
+        "answer_4": "Frischkäse",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie viele Inseln hat Indonesien insgesamt?",
+        "answer_1": "etwa 150",
+        "answer_2": "etwa 300",
+        "answer_3": "etwa 4000",
+        "answer_4": "über 17000",
+        "right_answer": 4
+    },
 ];
 
 let currentQuestion = 0;
@@ -51,12 +107,36 @@ function answer(selection) {
     console.log('selectedQuestion in Char is: ', selectedQuestionNumber)
     console.log('Right answer is: ', question['right_answer'])
 
+
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
     if (selectedQuestionNumber == question['right_answer']) {
         console.log('Richtige Antwort Wow!!')
         document.getElementById(selection).parentNode.classList.add('bg-success'); //bei richtiger Antwort mit der ID-seine Eltern Container wird background zur grünen Farbe geändert
     } else {
         console.log('Leider falsche Antwort!!')
         document.getElementById(selection).parentNode.classList.add('bg-danger'); //bei falscher Antwort mit der ID-seine Eltern Container wird background zur rote Farbe geändert
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+    document.getElementById('next-button').disabled =false;
 }
 
+
+function nextQuestion(){
+    currentQuestion++; // z.B. Variable von 0 auf 1 erhöhen
+    resetAnswerButtons();
+    showQuestion();
+    document.getElementById('next-button').disabled =ture;
+}
+
+//alle Antworten von Backgroud-Color entfernen
+function resetAnswerButtons(){  
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
